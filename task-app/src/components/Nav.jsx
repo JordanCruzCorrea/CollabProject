f5124a4dce700ff38d8e3034fef65ec05a6f4aee
wwhite11 <2winstonrwhite@gmail.com>
2019-12-18 10:23:19 -0500

    needsWork

--- task-app/src/components/Nav.jsx
+++ task-app/src/components/Nav.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Nav from 'react-bootstrap/Nav'
+import { NavLink } from 'react-router-dom'
 import { FaHome, FaUserCircle } from "react-icons/fa";
 import {  AiOutlineShoppingCart } from 'react-icons/ai';
 
--- task-app/src/screens/About.jsx
+++ task-app/src/screens/About.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const About = () => {
     return (
         <div>
-            <h1>Hello World</h1>
+            <h1>Hello About World</h1>
             </div>
     )
 }
