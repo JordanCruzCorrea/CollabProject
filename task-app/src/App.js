caf4be78a6edec6e6545c0ea4393fde734028151
wwhite11 <2winstonrwhite@gmail.com>
2019-12-17 19:29:55 -0500

    not finished

--- task-app/src/App.js
+++ task-app/src/App.js
@@ -1,20 +1,9 @@
 import React from 'react';
-<<<<<<< HEAD
-import './App.css';
-=======
-
-import './styles/css/App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
->>>>>>> 2c6ddf910b9149fc6630a725caa0c13aba896660
+import './styles/css/App.css'
 
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
-      <h1>Hello world</h1>
-=======
-      
->>>>>>> 2c6ddf910b9149fc6630a725caa0c13aba896660
     </div>
   );
 }
